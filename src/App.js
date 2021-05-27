08a94168d8241013ecbb86a1536004a96c8b4d37
rwxgit <10249739+rwxgit@users.noreply.github.com>
2021-05-27 22:12:37 +0530

    end of day - 27th May 2021

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import './App.css';
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import "./App.css";
+import Atm from "./pages/Atm";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h2>my new app</h2>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/atm">
+          <Atm />
+        </Route>
+        {/* <Route path="downloadfile">
+
+        </Route>
+        <Route path="downloadfiles">
+
+        </Route>
+        <Route path="testani">
+
+        </Route>
+        <Route path="testccni">
+
+        </Route>
+        <Route path="parsetrace">
+
+        </Route>
+        <Route path="parselog">
+
+        </Route> */}
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Atm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Atm = () => {
+    return (
+        <div>
+            <h2>ATM page</h2>
+        </div>
+    )
+}
+
+export default Atm
